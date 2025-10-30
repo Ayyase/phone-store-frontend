96d34e714a8e4a8b0f5006bc5a4e0b8bfda1171c
Yahya Ayyash <ayyash479@gmail.com>
2025-10-30 14:33:41 +0700

    first commit

---
+++ src/pages/dashboard/dasboardUser/dashboardUser.jsx
@@ -0,0 +1,38 @@
+import DataTable from "@/components/dataTable";
+import { columns } from "@/pages/dashboard/dasboardUser/columsUser";
+import LayoutDashboard from "@/components/layout/layoutDashboard";
+import { getUsers } from "@/utils/api/users";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import AddUser from "./addUser";
+
+export default function DashboardUser() {
+  const [users, setUsers] = useState([]);
+  const [addUserPopup, setAddUserPopup] = useState(false);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await getUsers();
+      setUsers(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <LayoutDashboard>
+      {addUserPopup && <AddUser setAddUserPopup={setAddUserPopup} />}
+      <Button
+        onClick={() => setAddUserPopup(true)}
+        className="cursor-pointer mb-4"
+      >
+        Add User
+      </Button>
+      <DataTable columns={columns} data={users} />
+    </LayoutDashboard>
+  );
+}
